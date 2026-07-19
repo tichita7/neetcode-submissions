037d9c1d53484e76bd7fe35e66828595b44aee60
Tichita <111648697+tichita7@users.noreply.github.com>
2026-07-19 15:30:18 +0530

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+//Using map and min heap to group elements with frequencies
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        int n = nums.size();
+        
+        unordered_map<int,int> mp;
+
+        for(auto it: nums){
+            mp[it]++;
+        }
+
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq; //bcz we need to eliminate the min first
+
+        for(auto& it: mp){
+            pq.push({it.second, it.first});
+
+            if(pq.size() > k){
+                pq.pop();
+            }
+        }
+
+        vector<int> ans;
+        while(!pq.empty()){
+            auto p = pq.top();
+            ans.push_back(p.second);
+            pq.pop();
+        }
+
+        return ans;
+
+    }
+};
